43781a8cfd50a3d0d7ff4022da80aa588f6ad561
MacReiben <macreiben@gmail.com>
2023-12-17 11:57:18 +0100

    steam cmd hardcoded, because unable to pass it from argument for now.

--- src/download-item-workshop.ps1
+++ src/download-item-workshop.ps1
@@ -5,12 +5,12 @@ param([Parameter(Mandatory)] [string] $SteamCMDFolder,
   [bool] $Force = $false,
   [string] $SteamCmdEntryPoint = "steamcmd")
 
-Write-Output "====== DownloadItem script ======"
-Write-Output "Given parameter SteamCmdPath:         $SteamCMDFolder"
-Write-Output "Given parameter WorkshopItems:        $WorkshopItems"
-Write-Output "Given parameter ItemsIdFilePath:      $ItemsIdFilePath"
-Write-Output "Given parameter Force:                $Force"
-Write-Output "Given parameter Diagnose:             $Diagnose"
+Write-Host "====== DownloadItem script ======"
+Write-Host "Given parameter SteamCmdPath:         $SteamCMDFolder"
+Write-Host "Given parameter WorkshopItems:        $WorkshopItems"
+Write-Host "Given parameter ItemsIdFilePath:      $ItemsIdFilePath"
+Write-Host "Given parameter Force:                $Force"
+Write-Host "Given parameter Diagnose:             $Diagnose"
 
 $originalLocation = Get-Location 
 
@@ -23,16 +23,16 @@ if ($is_steamCmdFolder_exist -ne $true) {
 
 $items = $WorkshopItems;
 
-if ($ItemsIdFilePath -ne $null) {
+# if ($ItemsIdFilePath -ne $null) {
 
-  Write-Output "Using file instead of inline parameters (row count processed):"
+#   Write-Output "Using file instead of inline parameters (row count processed):"
 
-  $items = New-Object System.Collections.ArrayList
+#   $items = New-Object System.Collections.ArrayList
 
-  foreach ($itemId in Get-Content $ItemsIdFilePath) {
-    $items.Add($itemId)
-  }
-}
+#   foreach ($itemId in Get-Content $ItemsIdFilePath) {
+#     $items.Add($itemId)
+#   }
+# }
 
 Set-Location $SteamCMDFolder
 
@@ -44,20 +44,20 @@ foreach ($item in $items) {
     
   $is_workshop_item_in_local_cmd = Test-Path $workshop_item_in_local_cmd
 
-  Write-Output "Computed value workshop_item_in_local_cmd:         $workshop_item_in_local_cmd"
-  Write-Output "Computed value is_workshop_item_in_local_cmd:      $is_workshop_item_in_local_cmd"
+  Write-Host "Computed value workshop_item_in_local_cmd:         $workshop_item_in_local_cmd"
+  Write-Host "Computed value is_workshop_item_in_local_cmd:      $is_workshop_item_in_local_cmd"
 
   if ($is_workshop_item_in_local_cmd -eq $false -or $Force) {
 
     if ($Diagnose -eq $false) {
-      ./$SteamCmdEntryPoint +login anonymous +workshop_download_item 365960 $item +quit
+      /steamcmd/steamcmd.sh +login anonymous +workshop_download_item 365960 $item +quit
     }
   }
   else {
-    Write-Output "Item [$item] already downloaded. Use -Force to replace."
+    Write-Host "Item [$item] already downloaded. Use -Force to replace."
   }
 }
 
 Set-Location $originalLocation
 
-Write-Output "====== DownloadItem script ====== DONE"
+Write-Host "====== DownloadItem script ====== DONE"
