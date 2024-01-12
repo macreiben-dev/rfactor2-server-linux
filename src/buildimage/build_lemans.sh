8a16dc9334b12acdf094fadc4031607bbcebb738
Christian Finel <macreiben@gmail.com>
2024-01-12 22:36:32 +0100

    remove endofline windows.

--- src/buildimage/build_lemans.sh
+++ src/buildimage/build_lemans.sh
@@ -1,4 +1,3 @@
 items="1737056865,2331284105,1737056920,1411394979,1411402194,2103827595"
 vncPassword="yourpassword"
-
-docker build . -t "rf2-linux-server-lemans" --build-arg workshopitemList=$items --build-arg vncPassword=$vncPassword --progress plain
+docker build . -t "rf2-linux-server-lemans" --no-cache --build-arg workshopitemList=$items
