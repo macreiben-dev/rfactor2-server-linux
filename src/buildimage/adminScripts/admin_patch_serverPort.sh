c1f8eb05f405ea1d89e53d6a5f275d0f5fb908cb
Christian Finel <macreiben@gmail.com>
2024-01-17 19:43:07 +0100

    remove unused code.

--- src/buildimage/adminScripts/admin_patch_serverPort.sh
+++ src/buildimage/adminScripts/admin_patch_serverPort.sh
@@ -14,23 +14,13 @@ fi
 
 # Check if the variable is set
 if [ -n "$http_server_port" ] && [ -n "$simulation_port" ] ; then
-    
-    # Clean previously generated files
-    rm /server/UserData/player/*.tmp
 
-    # Create a backup of the original file
-    cp "$json_file" "${json_file}.tmp"
-
-    # Update the JSON file using jq
-
+    # Update the JSON configuration file
     sed -i 's/\("Simulation Port":\)[0-9]\+/\1'"$simulation_port"'/' "${json_file}"
     sed -i 's/\("HTTP Server Port":\)[0-9]\+/\1'"$http_server_port"'/' "${json_file}"
-
-    # Replace configuration
-    mv "${json_file}_001.tmp" "${json_file}"
 
     echo "HTTP Server Port updated to $http_server_port in $json_file"
     echo "Simulation Port updated to $simulation_port in $json_file"
 else
-    echo "HTTP_SERVER_PORT environment variable is not set."
+    echo "HTTP_SERVER_PORT or SIMULATION_PORT environment variable is not set."
 fi
