70949823f28ef52349f5b8410dab52b628d2a7f8
Christian Finel <macreiben@gmail.com>
2024-01-23 23:37:38 +0100

    suffic le mans image with -endu.

--- src/buildimage/build_lemans_endu.sh
+++ src/buildimage/build_lemans_endu.sh
@@ -1,3 +1,3 @@
 items="1737056865,2331284105,1737056920,1411394979,1411402194,2103827595"
 vncPassword="yourpassword"
-docker build . -t "rf2-linux-server-lemans" --no-cache --build-arg workshopitemList=$items
+docker build . -t "rf2-linux-server-lemans-endu" --no-cache --build-arg workshopitemList=$items
