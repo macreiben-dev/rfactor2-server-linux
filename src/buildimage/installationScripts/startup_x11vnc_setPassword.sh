b77a97f9c4bf98a8a6a918d36491e8360b7d85d4
Christian Finel <macreiben@gmail.com>
2024-01-29 16:16:17 +0100

    remove because chown is made in docker file.

--- src/buildimage/installationScripts/startup_x11vnc_setPassword.sh
+++ src/buildimage/installationScripts/startup_x11vnc_setPassword.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RF2USER_HOME="/home/rf2user"
+RF2USER_HOME="/root"
 VNC_PASSWORD_PATH="$RF2USER_HOME/.vnc/passwd"
 
 if [ -z "$VNC_PASSWORD" ]; then
@@ -10,6 +10,3 @@ else
   echo "Setting VNC password - ${VNC_PASSWORD} - from environment variable."
   echo "$VNC_PASSWORD" | x11vnc -storepasswd $VNC_PASSWORD $VNC_PASSWORD_PATH
 fi
-
-# Set ownership
-chown -R rf2user:rf2user $RF2USER_HOME
