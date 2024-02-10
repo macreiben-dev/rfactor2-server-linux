432267b527b3e579ad2d86c3eaabf1c527fb9ed8
Christian Finel <macreiben@gmail.com>
2024-02-10 19:28:19 +0100

    file moved.

--- src/buildimage/installationScripts/step_x11vnc_configure.sh
+++ src/buildimage/installationScripts/step_x11vnc_configure.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 mkdir -p $RUNTIME_USER_HOME/.vnc
-
-# cp /etc/X11/Xwrapper.config /etc/X11/Xwrapper.config.bak
-
-# sed -i 's/^allowed_users=console$/allowed_users=anybody/' /etc/X11/Xwrapper.config
