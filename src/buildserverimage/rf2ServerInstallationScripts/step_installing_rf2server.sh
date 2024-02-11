fda96963391f3079b04b89d201341fd7beb2dd4e
Christian Finel <macreiben@gmail.com>
2024-02-11 23:05:43 +0100

    docker split completed.

--- src/buildserverimage/rf2ServerInstallationScripts/step_installing_rf2server.sh
+++ src/buildserverimage/rf2ServerInstallationScripts/step_installing_rf2server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /server \
-  && cd /server \
-  && /steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /server +login anonymous +app_update 400300 validate +quit \
-  && chown -R $RUNTIME_USER_ACCOUNT:$RUNTIME_USER_ACCOUNT /server \
+mkdir /server
+cd /server
+/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /server +login anonymous +app_update 400300 validate +quit
+# chown -R $RUNTIME_USER_ACCOUNT:$RUNTIME_USER_ACCOUNT /server
