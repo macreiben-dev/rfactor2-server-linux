70d4c8bbd4cbda4a5def045bdf619b05a835fb3e
Christian Finel <macreiben@gmail.com>
2024-03-17 14:41:54 +0100

    startup is working fine now.

--- src/02_buildimage/installationScripts/startup.sh
+++ src/02_buildimage/installationScripts/startup.sh
@@ -4,7 +4,4 @@
 sh /scripts/startup_x11vnc_setPassword.sh
 
 # Start x server
-x11vnc -forever -usepw -create -o ~/.vnc/x11vnc.log && cd /server
-
-# Symbolic link to x11vnc.log
-ln -s  ~/.vnc/x11vnc.log /mnt/host-monitored/x11vnc.log
+x11vnc -forever -usepw -create -o /mnt/host-monitored/x11vnc.log && cd /server
