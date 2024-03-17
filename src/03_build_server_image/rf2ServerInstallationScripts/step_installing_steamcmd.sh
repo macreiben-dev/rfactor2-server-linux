aa434d66544991828d673492f4ca19b3e704382b
Christian Finel <macreiben@gmail.com>
2024-03-17 13:44:07 +0100

    installation script folder renamed.

--- src/03_build_server_image/rf2ServerInstallationScripts/step_installing_steamcmd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /steamcmd \
-  && cd /steamcmd \
-  && wget "https://media.steampowered.com/client/steamcmd_linux.tar.gz" \
-  && tar -xvzf steamcmd_linux.tar.gz \
-  && ./steamcmd.sh +quit
