e9efd42daf0e380179ecc6794582b4b2a64e4342
Christian Finel <macreiben@gmail.com>
2024-03-17 14:39:42 +0100

    output log removed from command line.

--- src/02_buildimage/installationScripts/startup_x11vnc_setPassword.sh
+++ src/02_buildimage/installationScripts/startup_x11vnc_setPassword.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-RF2USER_HOME=$RUNTIME_USER_HOME
+VNC_PASSWORD_PATH=~/.vnc/passwd
 
-VNC_PASSWORD_PATH="$RF2USER_HOME/.vnc/passwd"
-
-VNC_LOG_FOLDER="/var/log/x11vnc"
-VNC_LOG_FILE="$VNC_LOG_FOLDER/x11vnc.log"
-
-mkdir $RF2USER_HOME/.vnc
-mkdir -p $VNC_LOG_FOLDER
+mkdir -p ~/.vnc
 
 echo "Given runtime user is: " & whoami
-echo "Given runtime user home: $RF2USER_HOME"
 echo "Computed VNC password path: $VNC_PASSWORD_PATH"
-echo "Computed VNC log file: $VNC_LOG_FILE"
 
 LOCAL_PASSWORD="yourpassword"
 
@@ -22,4 +14,5 @@ if [ -n "$VNC_PASSWORD" ]; then
 fi
 
 echo "Used local password: $LOCAL_PASSWORD"
- x11vnc -storepasswd $LOCAL_PASSWORD $VNC_PASSWORD_PATH -o $VNC_LOG_FILE
+
+x11vnc -storepasswd $LOCAL_PASSWORD $VNC_PASSWORD_PATH
