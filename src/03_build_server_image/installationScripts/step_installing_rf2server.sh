494d046cf1a6decd263f0d81e450ca4f50907db7
Christian Finel <macreiben@gmail.com>
2024-03-17 15:17:37 +0100

    remove commented line on installing server step.

--- src/03_build_server_image/installationScripts/step_installing_rf2server.sh
+++ src/03_build_server_image/installationScripts/step_installing_rf2server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir /server
+mkdir -p /server
 cd /server
 /steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /server +login anonymous +app_update 400300 validate +quit
-# chown -R $RUNTIME_USER_ACCOUNT:$RUNTIME_USER_ACCOUNT /server
