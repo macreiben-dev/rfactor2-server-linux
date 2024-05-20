27a771f4a551999b8937c45bdc6ab4d7148299b0
Christian Finel <macreiben@gmail.com>
2024-05-20 16:51:39 +0200

    refactoring on build scripts and le mans added.

--- src/03_build_server_image/build_indianapolis_gte.sh
+++ src/03_build_server_image/build_indianapolis_gte.sh
@@ -1,7 +1,8 @@
 cars="1411387394,1411394979,1411402133,1737056920,1411394979,2103827595,1411402194,2331284205,2331284105,2883380705"
 
-tracks="1737056865"
+# s397 indianapolis
+tracks="917386941"
 
 items="${cars},${tracks}"
 
-docker build . -t "rf2-linux-server-lemans-endu" --build-arg workshopitemList=$items
+docker build . -t "rf2-linux-server-indianapolis-gte" --build-arg workshopitemList=$items
--- src/03_build_server_image/build_lemans_gte.sh
+++ src/03_build_server_image/build_lemans_gte.sh
@@ -1,6 +1,7 @@
 cars="1411387394,1411394979,1411402133,1737056920,1411394979,2103827595,1411402194,2331284205,2331284105,2883380705"
-tracks="2883380016"
+
+tracks="1737056865"
 
 items="${cars},${tracks}"
 
-docker build . -t "rf2-linux-server-barhain-endu" --build-arg workshopitemList=$items
+docker build . -t "rf2-server-linux-lemans-gte" --build-arg workshopitemList=$items
