3975f4d1a889fdd014a9f9f53e4cbd416286e1eb
Christian Finel <macreiben@gmail.com>
2024-10-29 18:54:23 +0100

    startup script patched so that xfce starts well.

--- src/02_buildimage/installationScripts/startup.sh
+++ src/02_buildimage/installationScripts/startup.sh
@@ -11,8 +11,15 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 # Start Xvfb on display :0 with screen 0, resolution 1024x768x24
 Xvfb :0 -screen 0 1024x768x24 &
+export DISPLAY=:0
 
 # Wait for Xvfb to start
+sleep 5
+
+# Start XFCE session
+startxfce4 &
+
+# Wait for XFCE to start
 sleep 5
 
 x11vnc -v -forever -usepw -display :0 -auth /root/.Xauthority
