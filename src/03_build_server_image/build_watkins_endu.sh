0a56fb50ceb5f9f047802ac053046b671c5395e9
Christian Finel <macreiben@gmail.com>
2024-10-30 12:33:11 +0100

    added lmh and p2

--- src/03_build_server_image/build_watkins_endu.sh
+++ src/03_build_server_image/build_watkins_endu.sh
@@ -11,12 +11,17 @@ gt3_astonmartin="1596822282"
 gt3_ferrari488="2103827617"
 gt3_bmwm4="2741380074"
 
-# hypercar=""
-# lmp2=""
+lmh_ferrari="3088947577"
+lmh_porsche="3088949297"
+
+p2_oreca="1411402194"
+
+hypercar="${lmh_ferrari},${lmh_porsche}"
+lmp2="${p2_oreca}"
 gt3="${gt3_bentley},${gt3_c7r},${gt3_mercedes},${gt3_porsche911},${gt3_mclaren_720s},${gt3_bmwm6},${gt3_audilms},${gt3_astonmartin},${gt3_ferrari488},${gt3_bmwm4}"
 
 tracks="1508582012"
-cars="$gt3"
+cars="$gt3,$hypercar,$lmp2"
 
 items="${cars},${tracks}"
 
